1af5f16c98f2f960a3afa0b27ad2a3863dfb812c
xushengfeng <xushengfeng_zg@163.com>
2024-02-26 00:44:03 +0800

    标记 自动忽略重新排序

--- src/js.ts
+++ src/js.ts
@@ -2733,13 +2733,19 @@ async function autoIgnore() {
     const dialog = el("dialog", { class: "words_select" }) as HTMLDialogElement;
     const f = el("div");
     const studyWords = await wordsStore.keys();
-    const newWords = words.filter((w) => !oldWords.includes(w)).filter((w) => !oldWords.includes(lemmatizer(w)));
-    const sourceWords = newWords.map((w) => lemmatizer(w));
-    const newWords1 = Array.from(new Set(sourceWords.concat(newWords))).filter((w) => !studyWords.includes(w));
-    newWords1.forEach((w) => {
-        let item = el("label", [el("input", { type: "checkbox", value: w }), w]);
-        f.append(item);
-    });
+    const newWords = words.filter((w) => !oldWords.includes(w));
+    const newWords1: string[] = [];
+    for (let w of newWords) {
+        newWords1.push(lemmatizer(w), w);
+    }
+    Array.from(new Set(newWords1))
+        .filter((w) => !studyWords.includes(w))
+        .filter((w) => !oldWords.includes(w))
+        .filter((w) => w.length > 1)
+        .forEach((w) => {
+            let item = el("label", [el("input", { type: "checkbox", value: w }), w]);
+            f.append(item);
+        });
     dialog.append(
         f,
         el("button", iconEl(ok_svg), {
