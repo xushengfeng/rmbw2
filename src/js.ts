cf352bc0215898ea94c06546e9ebee0597fc1d5a
xushengfeng <xushengfeng_zg@163.com>
2025-02-02 22:35:46 +0800

    阅读 默认添加词为词根

--- src/js.ts
+++ src/js.ts
@@ -3072,7 +3072,7 @@ async function showNormalBook(book: Book, s: Section) {
                     // 故句子一定有至少一个元素，且不全是空格，至少能匹配到此单词
 
                     const id = await saveCard({
-                        key: (span.getAttribute("data-t") as string).toLowerCase(),
+                        key: lemmatizer((span.getAttribute("data-t") as string).toLocaleLowerCase()),
                         index: [Number(span.getAttribute("data-s")), Number(span.getAttribute("data-e"))] as TxtSlice,
                         cindex: [s, e] as TxtSlice,
                     });
