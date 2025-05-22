45f4638345489cffe48511024b5f9afc715f8fea
xushengfeng <xushengfeng_zg@163.com>
2025-05-22 16:35:41 +0800

    复习 单词 显示答案只触发一次

--- src/js.ts
+++ src/js.ts
@@ -6372,6 +6372,7 @@ async function showWordReview(x: { id: string; card: Card }, isAi: boolean) {
     reviewHotkey.show.f = () => {
         showAnswer();
         buttons.finish();
+        reviewHotkey.show.f = () => {};
     };
     const dic = view().on("click", reviewHotkey.show.f);
     const buttons = getReviewCardButtons(x.id, x.card, contextText, async (type, first) => {
